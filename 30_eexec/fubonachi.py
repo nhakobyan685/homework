6c17417c82b0f688f62523ee97a04efb74577608
nhakobyan685 <118097522+nhakobyan685@users.noreply.github.com>
2023-02-18 11:23:07 +0400

    Create fubonachi.py

---
+++ 30_eexec/fubonachi.py
@@ -0,0 +1,13 @@
+#!/bin/bash/python3
+# def fibonacci_memoized(n, cache={}):
+#     if n in cache:
+#         return cache[n]
+#     if n <= 1:
+#         result = n
+#     else:
+#         result = fibonacci_memoized(n-1) + fibonacci_memoized(n-2)
+#     cache[n] = result
+#     return result
+
+# result = fibonacci_memoized(10)
+# print(result)
